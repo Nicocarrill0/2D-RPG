4f934af13ab22d2a8d2ee74cb023c3a0a74939bf
Nicocarrill0 <128573090+Nicocarrill0@users.noreply.github.com>
2024-11-18 22:54:32 -0800

    Modified GamePanel

--- src/com/nico/game/GamePanel.java
+++ src/com/nico/game/GamePanel.java
@@ -39,22 +39,89 @@ public class GamePanel extends JPanel implements Runnable{
         g = (Graphics2D) img.getGraphics();
     }
 
+
+
     public void run() {
     init();
 
+    final double GAME_HERTZ = 60.0;
+    final double TBU = 1000000000/GAME_HERTZ;
+
+    final int MUBR = 5;
+
+    double lastUpdateTime = System.nanoTime();
+    double lastRenderTime;
+
+    final double TARGET_FPS = 60;
+    final double TTBR = 1000000000/TARGET_FPS;
+
+    int frameCount = 0;
+    int lastSecondTime = (int) (lastUpdateTime / 1000000000);
+    int oldFrameCount = 0;
+
     while(running) {
-        update();
+
+        double now = System.nanoTime();
+        int updateCount = 0;
+        while(((now - lastUpdateTime) > TBU) && (updateCount < MUBR)) {
+                update();
+                input();
+                lastUpdateTime += TBU;
+                updateCount++;
+        }
+
+        if(now - lastUpdateTime > TBU) {
+            lastUpdateTime = now - TBU;
+        }
+
+        input();
         render();
         draw();
+        lastRenderTime = now;
+        frameCount++;
+
+        int thisSecond = (int) (lastUpdateTime / 1000000000);
+
+        if(thisSecond > lastSecondTime) {
+            if(frameCount != oldFrameCount) {
+                System.out.println("NEW SECOND" + thisSecond + " " + frameCount);
+                oldFrameCount = frameCount;
+            }
+            frameCount = 0;
+            lastSecondTime = thisSecond;
+        }
+
+        while(now - lastRenderTime < TTBR && now - lastUpdateTime < TBU) {
+            Thread.yield();
+
+            try{
+                Thread.sleep(1);
+            }catch(Exception e){
+                System.out.println("ERROR: yielding thread");
+            }
+            now = System.nanoTime();
+            }
+        }
     }
-    }
+
 
     private int x = 0;
 
-    public void update() {
+    public void update() {}
 
+    public void input() {}
+
+    public void render() {
+        if(g != null) {
+            g.setColor(new Color(66,134,244));
+            g.fillRect(0, 0, width, height);
+        }
     }
-    public void draw() {}
-    public void render() {}
+    public void draw() {
+        Graphics g2 = (Graphics) this.getGraphics();
+        g2.drawImage(img, 0, 0, width, height, null);
+        g2.dispose();
+    }
+
 
 }
